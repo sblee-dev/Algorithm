326c87a72478da342b5eb257d73b96ca44e3f29d
sblee-dev <59276228+sblee-dev@users.noreply.github.com>
2023-03-02 17:41:39 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,11 +1,12 @@
 class Solution {
     public int solution(int n) {
-        return n / GCD(n, 6);
-    }
-    
-    public int GCD(int num1, int num2) {
-        if (num1 % num2 == 0)
-            return num2;
-        return GCD(num2, num1 % num2);
+        int answer = 1;
+
+        while(true){
+            if(6*answer%n==0) break;
+            answer++;
+        }
+
+        return answer;
     }
 }
