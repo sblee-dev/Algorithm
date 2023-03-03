a274099087374074d93988be16ce360b4ea0f634
sblee-dev <59276228+sblee-dev@users.noreply.github.com>
2023-03-03 10:35:52 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,16 +1,11 @@
 class Solution {
     public int solution(int price) {
         int answer = 0;
-        
-        if ( price >= 500000 ) {
-            price *= 0.8;
-        } else if ( price >= 300000 ) {
-            price *= 0.9;
-        } else if ( price >= 100000 ) {
-            price *= 0.95;
-        }
-        answer = price;
-        
-        return answer;
+
+        if(price>=500000) return (int)(price*0.8);
+        if(price>=300000) return (int)(price*0.9);
+        if(price>=100000) return (int)(price*0.95);
+
+        return price;
     }
 }
