9dcdcb33ec4eb3a9002a1ebade6a150b50b75758
sblee-dev <59276228+sblee-dev@users.noreply.github.com>
2023-03-03 13:31:58 +0900

    [level 0] Title: 배열 뒤집기, Time: 3.16 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,9 +1,13 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length];
-        for (int i=0; i<num_list.length; i++) {
-            answer[num_list.length-1-i] = num_list[i];
-        }
-        return answer;
+        List<Integer> list = Arrays.stream(num_list).boxed().collect(Collectors.toList());
+
+        Collections.reverse(list);
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
