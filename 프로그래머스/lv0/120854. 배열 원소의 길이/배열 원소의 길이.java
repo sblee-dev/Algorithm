2fa8e5c903876b559b86be1c32d07f498aac5c09
sblee-dev <59276228+sblee-dev@users.noreply.github.com>
2023-03-10 22:08:13 +0900

    [level 0] Title: 배열 원소의 길이, Time: 2.46 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -1,11 +1,6 @@
+import java.util.Arrays;
 class Solution {
     public int[] solution(String[] strlist) {
-        int[] answer = new int[strlist.length];
-        int i = 0;
-        for (String str : strlist) {
-            answer[i] = str.length();
-            i++;
-        }
-        return answer;
+        return Arrays.stream(strlist).mapToInt(String::length).toArray();
     }
 }
