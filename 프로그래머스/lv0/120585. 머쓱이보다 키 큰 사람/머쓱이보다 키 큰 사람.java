f8a58f50106fd1daa3e55dd4d6923459dc01f7b2
sblee-dev <59276228+sblee-dev@users.noreply.github.com>
2023-03-12 23:16:28 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 1.02 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,12 +1,6 @@
 import java.util.Arrays;
 class Solution {
     public int solution(int[] array, int height) {
-        Arrays.sort(array);
-        int index = Arrays.binarySearch(array, height);
-        if (index < 0) {
-            return array.length + index + 1;
-        } else {
-            return array.length - index - 1;
-        }
+        return (int) Arrays.stream(array).filter(val -> val > height).count();
     }
 }
