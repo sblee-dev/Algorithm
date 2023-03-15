0a0360f1779403e3a976c616739ee89b489204da
sblee-dev <59276228+sblee-dev@users.noreply.github.com>
2023-03-15 16:38:47 +0900

    [level 0] Title: 가위 바위 보, Time: 1.96 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,16 +1,9 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
 class Solution {
     public String solution(String rsp) {
-        String[] str = rsp.split("");
-        String result = "";
-        for (String s : str) {
-            if (s.equals("2")) {
-                result += "0";    
-            } else if (s.equals("0")) {
-                result += "5";
-            } else {
-                result += "2";
-            }
-        }
-        return result;
+        return Arrays.stream(rsp.split("")).map(i -> i.equals("2")?"0":i.equals("0")?"5":"2").collect(Collectors.joining());
+            
+            
     }
 }
