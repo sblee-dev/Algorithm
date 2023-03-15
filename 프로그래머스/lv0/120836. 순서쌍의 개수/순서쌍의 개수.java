1e0608ddc1d07fb4e4e061dc6183938528f62f42
sblee-dev <59276228+sblee-dev@users.noreply.github.com>
2023-03-15 14:35:33 +0900

    [level 0] Title: 순서쌍의 개수, Time: 8.57 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,10 +1,12 @@
-import java.lang.Math;
+import java.util.stream.IntStream;
 class Solution {
     public int solution(int n) {
-        int cnt = 0;
-        for (int i=1; i<=Math.sqrt(n); i++) {
-            if ( n % i == 0 ) cnt += n/i == i ? 1 : 2;
-        }
-        return cnt;
+        return (int) IntStream.rangeClosed(1, n).filter(i -> n%i==0).count();
+        
+        // int cnt = 0;
+        // for (int i=1; i<=Math.sqrt(n); i++) {
+        //     if ( n % i == 0 ) cnt += n/i == i ? 1 : 2;
+        // }
+        // return cnt;
     }
 }
