a181b9bca322a6a5717ee585c0d9711cee668b79
sblee-dev <59276228+sblee-dev@users.noreply.github.com>
2023-03-15 11:17:15 +0900

    [level 0] Title: 진료 순서 정하기, Time: 4.68 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -3,15 +3,25 @@ import java.util.Comparator;
 import java.util.stream.Collectors;
 class Solution {
     public int[] solution(int[] e) {
-        return 
-            Arrays.stream(e)
-            .map(i ->
-                    Arrays.stream(e)
-                 .boxed()
-                 .sorted(Comparator.reverseOrder())
-                 .collect(Collectors.toList())
-                 .indexOf(i) + 1
-                )
-            .toArray();
+        int[] result = new int[e.length];
+        for (int i=0; i<e.length; i++) {
+            int cnt = 1;
+            for (int j=0; j<e.length; j++) {
+                if (e[i] < e[j]) cnt++;
+            }
+            result[i] = cnt;
+        }
+        return result;
+            
+            
+        // return Arrays.stream(e)
+        // .map(i ->
+        //         Arrays.stream(e)
+        //      .boxed()
+        //      .sorted(Comparator.reverseOrder())
+        //      .collect(Collectors.toList())
+        //      .indexOf(i) + 1
+        //     )
+        // .toArray();
     }
 }
