67b8883227051c85a9d3d95c728bb35ecb4a2906
sblee-dev <59276228+sblee-dev@users.noreply.github.com>
2023-03-18 17:07:41 +0900

    [level 0] Title: 편지, Time: 0.01 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120898. 편지/편지.java
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 class Solution {
     public int solution(String message) {
-        return (int) Arrays.stream(message.split("")).count() * 2;
+        return message.length() * 2;
     }
 }
