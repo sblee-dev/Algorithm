3e801892e2673f64b1b23fef3c2ae99ab3ebd89e
sblee-dev <59276228+sblee-dev@users.noreply.github.com>
2023-03-30 17:32:40 +0900

    [level 0] Title: 암호 해독, Time: 13.01 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -1,11 +1,9 @@
-import java.lang.StringBuilder;
-
 class Solution {
     public String solution(String cipher, int code) {
-        StringBuilder sb = new StringBuilder();
+        String answer = "";
         for (int i=1; i<=cipher.length()/code; i++) {
-            sb.append(cipher.charAt(i*code-1));
+            answer += cipher.charAt(i*code-1);
         }
-        return sb.toString();
+        return answer;
     }
 }
