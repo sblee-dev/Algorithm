50609c8d8e27c10152003c09688267b9de7876b0
sblee-dev <59276228+sblee-dev@users.noreply.github.com>
2023-04-10 10:49:28 +0900

    [level 0] Title: 대문자와 소문자, Time: 2.67 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,14 +1,18 @@
-import java.lang.StringBuilder;
+// import java.lang.StringBuilder;
+import java.util.stream.Collectors;
 class Solution {
     public String solution(String my_string) {
-        StringBuilder sb = new StringBuilder();
-        for (char str : my_string.toCharArray()) {
-               if (str < 97) {
-                   sb.append((char) (str + 32));
-               } else {
-                   sb.append((char) (str - 32));
-               }
-        }
-        return sb.toString();
+        
+        return my_string.chars().mapToObj(operand -> String.valueOf((char) (Character.isLowerCase(operand) ? Character.toUpperCase(operand) : Character.toLowerCase(operand)))).collect(Collectors.joining());
+        
+        // StringBuilder sb = new StringBuilder();
+        // for (char str : my_string.toCharArray()) {
+        //        if (str < 97) {
+        //            sb.append((char) (str + 32));
+        //        } else {
+        //            sb.append((char) (str - 32));
+        //        }
+        // }
+        // return sb.toString();
     }
 }
