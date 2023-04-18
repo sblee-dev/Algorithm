4f5b710d117d5a9862848e3f52125da08ac712d4
sblee-dev <59276228+sblee-dev@users.noreply.github.com>
2023-04-18 17:55:16 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 1.85 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,6 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(String my_string) {
+        return Arrays.stream(my_string.replaceAll("[A-Z|a-z]", "").split("")).mapToInt(Integer::parseInt).sum();
+    }
+}
