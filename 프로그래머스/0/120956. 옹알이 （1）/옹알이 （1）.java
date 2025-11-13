d240501ebeb5bda661ba5dacac62cf7e40f14480
sblee-dev <59276228+sblee-dev@users.noreply.github.com>
2025-11-13 14:44:28 +0900

    [level 0] Title: 옹알이 (1), Time: 0.98 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -3,11 +3,11 @@ import java.util.*;
 class Solution {
     public int solution(String[] babbling) {
         int cnt = 0;
-        List<String> arr = new ArrayList<>(Arrays.asList("aya", "ye", "woo", "ma"));
+        String[] arr = new String[]{"aya", "ye", "woo", "ma"};
         
         for (int i=0; i<babbling.length; i++) {
-            for (int j=0; j<arr.size(); j++) {
-                babbling[i] = babbling[i].replace(arr.get(j), " ");    
+            for (int j=0; j<arr.length; j++) {
+                babbling[i] = babbling[i].replace(arr[j], " ");    
             }
             if ("".equals(babbling[i].trim())) cnt++;
         }
