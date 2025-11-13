54eb57ea2ed6d2d7ce060bca54b1e62cc6abff6e
sblee-dev <59276228+sblee-dev@users.noreply.github.com>
2025-11-13 10:57:29 +0900

    [level 0] Title: 소인수분해, Time: 2.92 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                while (n % i == 0) {
+                    n = n / i;    
+                }
+                list.add(i);
+            }
+        }
+        
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
