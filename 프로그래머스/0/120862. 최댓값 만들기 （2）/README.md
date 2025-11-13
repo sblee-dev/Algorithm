f3e31a11404884c7295d747b4255e2855c273117
sblee-dev <59276228+sblee-dev@users.noreply.github.com>
2025-11-13 10:25:05 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.50 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최댓값 만들기 (2) - 120862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862?language=java) 
 
 ### 성능 요약
 
-메모리: 75.8 MB, 시간: 0.13 ms
+메모리: 82.8 MB, 시간: 0.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 13일 10:13:11
+2025년 11월 13일 10:25:08
 
 ### 문제 설명
 
--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,16 +1,8 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] numbers) {
-        int answer = numbers[0] * numbers[1];
-        
-        for (int i=0; i<numbers.length-1; i++) {
-            for (int j=i+1; j<numbers.length; j++) {
-                int num = numbers[i] * numbers[j];   
-                if (answer < num) {
-                    answer = num;
-                }
-            }
-        }
-        
-        return answer;
+        Arrays.sort(numbers);
+        return Math.max(numbers[0] * numbers[1], numbers[numbers.length - 1] * numbers[numbers.length - 2]);
     }
 }
