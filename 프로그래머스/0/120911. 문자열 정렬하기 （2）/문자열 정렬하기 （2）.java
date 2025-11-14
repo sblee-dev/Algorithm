2055ed6b48524ab33bd07192964a6720835fc493
sblee-dev <59276228+sblee-dev@users.noreply.github.com>
2025-11-14 16:16:16 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 4.16 ms, Memory: 94.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string) {
+        return Arrays.stream(my_string.toLowerCase().split("")).sorted().collect(Collectors.joining());
+    }
+}
